150056af306a81364820f958659267050d556c7a
zhouwangyiteng <zhouwang122@qq.com>
2015-10-26 22:39:28 +0800

    add base views of polls page

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,16 +3,16 @@ from .models import Question, Choice
 
 # Register your models here.
 class ChoiceInline(admin.TabularInline):
-	model = Choice
-	extra = 5
+    model = Choice
+    extra = 5
 		
 
 class QuestionAdmin(admin.ModelAdmin):
-	list_display = ('question_text', 'pub_date', 'was_published_recently')
-	fieldsets = [(None, {'fields': ['question_text']}), ('Date information', {'fields': ['pub_date']}), ]
-	inlines = [ChoiceInline]
-	list_filter = ['pub_date']
-	search_fields = ['question_text']
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    fieldsets = [(None, {'fields': ['question_text']}), ('Date information', {'fields': ['pub_date']}), ]
+    inlines = [ChoiceInline]
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Hello, world. you're at the polls index.")
+
+def detail(request, question_id):
+    return HttpResponse("You are looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You are looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You are voting on question %s." % question_id)
