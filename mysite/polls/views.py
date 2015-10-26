aeb58361f77f948c27afdafbc9ffb7ba4aa2997d
zhouwangyiteng <zhouwang122@qq.com>
2015-10-26 23:13:08 +0800

    add real models of polls

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,12 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+from django.http import Http404
+
+from .models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. you're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = RequestContext(request, {'latest_question_list': latest_question_list})
+    return HttpResponse(template.render(context))
 
 def detail(request, question_id):
-    return HttpResponse("You are looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You are looking at the results of question %s."
