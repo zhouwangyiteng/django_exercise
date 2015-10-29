a52db97b457993f56f98bdd590d3787ecbb9f80c
zhouwangyiteng <zhouwang122@qq.com>
2015-10-29 23:03:38 +0800

    使用通用视图

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,33 +3,32 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext, loader
 from django.http import Http404
 from django.core.urlresolvers import reverse
+from django.views import generic
 
 from .models import Question, Choice
 
 # Create your views here.
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = RequestContext(request, {'latest_question_list': latest_question_list})
-    return HttpResponse(template.render(context))
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
     p = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = p.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
-        return render(request, 'polls/detail.html', {
-            'question': p, 
-            'error_message': "You didn't select a choice.",
-            })
+        return render(request, 'polls/detail.html', {'question': p, 'error_message': "You didn't select a choice.",})
     else:
         selected_choice.votes += 1
         selected_choice.save()
