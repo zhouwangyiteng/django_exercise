4459c693e2ae0b2c2e28a98e68df3fc16f3776b7
zhouwangyiteng <zhouwang122@qq.com>
2015-11-24 23:03:25 +0800

    add models and views for register

---
+++ mysite/account/views.py
@@ -0,0 +1,36 @@
+#coding=utf-8
+
+from django.shortcuts import render
+from django import forms
+from django .shortcuts import render_to_response
+from django.http import HttpResponse, HttpResponseRedirect
+from django import RequestContext
+from account.models import User
+
+# Create your views here.
+
+class UserFrom(forms.Form):
+    username = forms.CharField(label='Username: ', max_length=100)
+    password = forms.CrarField(label='Password: ', widget=forms.PasswordInput())
+    email = forms.EmailField(label='Email: ')
+
+def register(request):
+    if request.method == 'POST':
+        uf = UserFrom(request.POST)
+        if uf.is_valid():
+            #get post information
+            username = uf.cleaned_data['username']
+            password = uf.cleaned_data['password']
+            email = uf.cleaned_data['email']
+            #write data into sql
+            user = User()
+            user.username = username
+            user.password = password
+            user.email = email
+            user.save()
+            #return successed page
+            return render_to_response('success.html', {'username': username})
+    else:
+        uf = UserFrom()
+    return render_to_response('register.html', {'uf': uf})
+
